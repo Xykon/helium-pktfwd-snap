9c9ae7f6ddf3dba3ee4e1c005b43c75c24cf27f9
Christian Ehlers <cehlers@pycom.io>
2021-09-26 12:25:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,31 +1,86 @@
-#! /bin/sh
+#!/bin/sh
 
-# Reset RAK2245 PIN
-SX1301_RESET_BCM_PIN=17
-echo "out" >/sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/direction
-echo "0"   >/sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "1"   >/sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
-sleep 0.1
-echo "0"   >/sys/class/gpio/gpio$SX1301_RESET_BCM_PIN/value
+#set -x
+#set -v
 
-cd $SNAP_COMMON
 
-if [ ! -e "local_conf.json" ]; then
-  if grep -q eth0 /proc/net/dev; then
-    DEV="eth0"
-  else
-    if grep -q wlan0 /proc/net/dev; then
-      DEV="wlan0"
-    else
-      echo "Error: no network device found, exiting !!!"
-	  exit 1
+IOT_SK_SX1301_RESET_PIN=23
+IOT_SK_SX1257_RESET_PIN=2
+IOT_SK_EN_PIN=13
+IOT_SK_FEM_PIN=25
+IOT_SK_CE0_PIN=8
+
+
+echo "Accessing concentrator reset pin through GPIO$IOT_SK_SX1301_RESET_PIN..."
+
+WAIT_GPIO() {
+    sleep 0.1
+}
+
+iot_sk_init() {
+    # setup GPIO 7
+    echo "$IOT_SK_SX1301_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
+    echo "$IOT_SK_SX1257_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
+    echo "$IOT_SK_EN_PIN" > /sys/class/gpio/export; WAIT_GPIO
+    echo "$IOT_SK_FEM_PIN" > /sys/class/gpio/export; WAIT_GPIO
+#    echo "$IOT_SK_CE0_PIN" > /sys/class/gpio/export; WAIT_GPIO
+
+    # set GPIO 7 as output
+    echo "out" > /sys/class/gpio/gpio$IOT_SK_EN_PIN/direction; WAIT_GPIO
+    echo "out" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/direction; WAIT_GPIO
+    echo "out" > /sys/class/gpio/gpio$IOT_SK_SX1257_RESET_PIN/direction; WAIT_GPIO
+    echo "out" > /sys/class/gpio/gpio$IOT_SK_FEM_PIN/direction; WAIT_GPIO
+#    echo "out" > /sys/class/gpio/gpio$IOT_SK_CE0_PIN/direction; WAIT_GPIO
+
+    # write output for SX1301 reset
+    echo "1" > /sys/class/gpio/gpio$IOT_SK_EN_PIN/value; WAIT_GPIO
+    echo "1" > /sys/class/gpio/gpio$IOT_SK_SX1257_RESET_PIN/value; WAIT_GPIO
+    echo "0" > /sys/class/gpio/gpio$IOT_SK_SX1257_RESET_PIN/value; WAIT_GPIO
+    echo "1" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/value; WAIT_GPIO
+    echo "0" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/value; WAIT_GPIO
+    echo "1" > /sys/class/gpio/gpio$IOT_SK_FEM_PIN/value; WAIT_GPIO
+#    echo "1" > /sys/class/gpio/gpio$IOT_SK_CE0_PIN/value; WAIT_GPIO
+#    echo "0" > /sys/class/gpio/gpio$IOT_SK_CE0_PIN/value; WAIT_GPIO
+
+    # set GPIO 7 as input
+#    echo "in" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/direction; WAIT_GPIO
+#    echo "in" > /sys/class/gpio/gpio$IOT_SK_SX1257_RESET_PIN/direction; WAIT_GPIO
+#    echo "in" > /sys/class/gpio/gpio$IOT_SK_CE0_PIN/direction; WAIT_GPIO
+}
+
+iot_sk_term() {
+    # cleanup GPIO 7
+    if [ -d /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN ]
+    then
+        echo "$IOT_SK_SX1301_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
     fi
-  fi
-  GW_ID="$(ip link show ${DEV} | awk '/ether/ {print $2}' | \
-	  awk -F\: '{print $1$2$3"FFFE"$4$5$6}' | \
-	  tr '[:lower:]' '[:upper:]')"
-  /usr/bin/echo -e "{\n\t\"gateway_conf\": {\n\t\t\"gateway_ID\": \"$GW_ID\",\n\t\t\"server_address\": \"127.0.0.1\",\n\t\t\"serv_port_up\": 1680,\n\t\t\"serv_port_down\": 1680\n\t}\n}" >local_conf.json
-fi
+    if [ -d /sys/class/gpio/gpio$IOT_SK_SX1257_RESET_PIN ]
+    then
+        echo "$IOT_SK_SX1257_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+    fi
+    if [ -d /sys/class/gpio/gpio$IOT_SK_EN_PIN ]
+    then
+        echo "$IOT_SK_EN_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+    fi
+    if [ -d /sys/class/gpio/gpio$IOT_SK_FEM_PIN ]
+    then
+        echo "$IOT_SK_FEM_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+    fi
+}
 
-$SNAP/bin/lora_pkt_fwd
+
+case "$1" in
+    start)
+    iot_sk_term
+    iot_sk_init
+    ;;
+    stop)
+    iot_sk_term
+    ;;
+    *)
+    echo "Usage: $0 {start|stop} [<gpio number>] [<gpio number>]"
+    exit 1
+    ;;
+esac
+
+exit 0
